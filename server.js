4e28235a4a704f429b855c68ff109726a6c39371
zwelethu <zwelethu@yahoo.com>
2019-11-24 10:54:00 +0200

    added controllers

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 //Load config file
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+app.get('/', (req, res) => {
+  res.send('<H1>Hi from express</H1>');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
