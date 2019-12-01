088e68c87b8053e461aae0fbf92b5cc0eb6bc1f8
zwelethu <zwelethu@yahoo.com>
2019-12-01 17:40:15 +0200

    early commit

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
+//logger file
+//dev logging middleware
 //Load config file
 dotenv.config({ path: './config/config.env' });
+connectDB();
 
 const app = express();
+
+//dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+//app.use(logger);
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
@@ -17,7 +28,16 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
